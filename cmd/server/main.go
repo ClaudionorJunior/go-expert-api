c75af3e76c721d8523e9e3c064007885ff19dbb2
ClaudionorJunior <devclaudionor@gmail.com>
2022-12-15 10:36:59 -0300

    feat: create user handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/ClaudionorJunior/go-expert-api/configs"
+	"github.com/ClaudionorJunior/go-expert-api/internal/dto"
+	"github.com/ClaudionorJunior/go-expert-api/internal/entity"
+	"github.com/ClaudionorJunior/go-expert-api/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	http.ListenAndServe(":8000", nil)
+
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewPoduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
